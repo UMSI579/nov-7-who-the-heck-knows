9f15194bf58bade6530d3ab0e9438b21e689fc33
Mullins, Benjamin <benm@umich.edu>
2024-11-02 19:48:01 -0400

    secret

---
+++ Secrets.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNAniv_BlRetarZW0hQYvgbW1EEZAwNf8",
+  authDomain: "chat-app-5d928.firebaseapp.com",
+  projectId: "chat-app-5d928",
+  storageBucket: "chat-app-5d928.firebasestorage.app",
+  messagingSenderId: "168224246360",
+  appId: "1:168224246360:web:664625eb7c09666ed0982d"
+};
