9a61b7b19f9ae5aff87b79626e774465f46b8ced
Mullins, Benjamin <benm@umich.edu>
2024-11-13 22:09:09 -0500

    display user lip

--- features/authSlice.js
+++ features/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { initializeApp, getApps } from 'firebase/app';
-import { setDoc, doc, getFirestore } from 'firebase/firestore';
+import { setDoc, doc, getFirestore, collection, onSnapshot } from 'firebase/firestore';
 import { firebaseConfig } from '../Secrets';
 
 let app;
@@ -11,6 +11,21 @@ if (apps.length == 0) {
   app = apps[0];
 }
 const db = getFirestore(app);
+
+
+let snapshotUnsubscribe = undefined;
+export const subscribeToUserUpdates = (dispatch) => {
+  if (snapshotUnsubscribe) {
+    snapshotUnsubscribe();
+  }
+    snapshotUnsubscribe = onSnapshot(collection(db, 'users'), usersSnapshot => {
+      const updatedUsers = usersSnapshot.docs.map(uSnap => {
+        console.log(uSnap.data());
+        return uSnap.data(); // already has key?
+      });
+      dispatch(loadUsers(updatedUsers));
+    });
+}
 
 export const addUser = createAsyncThunk(
   'chat/addUser',
@@ -30,6 +45,14 @@ export const addUser = createAsyncThunk(
 )
 
 
+export const loadUsers = createAsyncThunk(
+  'chat/loadUsers',
+  async (users) => {
+    return [...users];
+  }
+)
+
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
@@ -41,6 +64,10 @@ export const authSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(addUser.fulfilled, (state, action) => {
       state.users = [...state.users, action.payload]
+    });
+
+    builder.addCase(loadUsers.fulfilled, (state, action) => {
+      state.users = action.payload
     });
   }
 })
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,43 @@
 import {signOut, getAuthUser} from '../AuthManager';
 import { Button } from '@rneui/themed';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Alert, FlatList } from 'react-native';
+import {subscribeToUserUpdates} from "../features/authSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function HomeScreen({navigation}) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    subscribeToUserUpdates(dispatch)
+  }, []);
+
+
+  const users = useSelector(state => state.authSlice.users);
+  const currentAuthUser = getAuthUser();
+
   return (
     <View style={styles.container}>
       <Text>
         You're signed in { getAuthUser().displayName }!
       </Text>
+
+      <Text>
+        Here are your friends!
+      </Text>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={users}
+          renderItem={({item}) => {
+            if (item.key === currentAuthUser.uid) {
+              return (<View/>)
+            } else {
+              return (
+                <Text>{item.displayName}</Text>
+              )
+            }
+          }}
+        />
+      </View>
       <Button
         onPress={async () => {
           try {
@@ -30,6 +60,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'pink'
+  },
+
+  listContainer: {
+    flex: 0.5,
+    width: '100%',
   }
 });
 export default HomeScreen;
